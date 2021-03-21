ff649e78d0af973501b788c5abf52883fb85a262
noxymon <avicienna.ulhaq@gmail.com>
2021-03-21 22:10:30 +0700

    Initial Commit

---
+++ src/main/java/id/noxymon/gateway/proxygateway/ProxyGatewayApplication.java
@@ -0,0 +1,13 @@
+package id.noxymon.gateway.proxygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/noxymon/gateway/proxygateway/ProxyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package id.noxymon.gateway.proxygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
